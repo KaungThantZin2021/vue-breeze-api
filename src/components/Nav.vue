f8f6ad81fb655fbfa88726217de5d54ded666c9c
Kaung Thant Zin <86509255+KaungThantZin2021@users.noreply.github.com>
2024-04-20 16:46:51 +0630

    [Setup] components and routes

---
+++ src/components/Nav.vue
@@ -0,0 +1,5 @@
+<template>
+    <div>
+        <RouterLink to="/">Home</RouterLink> | <RouterLink to="/login">Login</RouterLink> | <RouterLink to="/register">Register</RouterLink>
+    </div>
+</template>
--- src/router/index.js
+++ src/router/index.js
@@ -2,8 +2,17 @@ import { createRouter, createWebHistory } from 'vue-router';
 
 const routes = [
     {
-        
-    }
+        path: '/register',
+        component: () => import('../components/Register.vue')
+    },
+    {
+        path: '/login',
+        component: () => import('../components/Login.vue')
+    },
+    {
+        path: '/',
+        component: () => import('../components/Home.vue')
+    },
 ];
 
 const router = createRouter({
